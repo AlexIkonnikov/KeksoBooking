a347119f0c66ae250750bd645f339c644ca56c62
Alex <sashaikonnikov55@gmail.com>
2020-11-22 00:21:26 +0700

    Added send function.

--- js/backend.js
+++ js/backend.js
@@ -1,1 +1,34 @@
 'use strict';
+
+(function() {
+
+    window.load = function (onError, onSuccses) {
+
+        let xhr = new XMLHttpRequest ();
+        xhr.open('get', 'https://javascript.pages.academy/keksobooking/data');
+        xhr.responseType = 'json';
+        xhr.timeout = 10000;
+
+        xhr.addEventListener('load', function() {
+
+            if (xhr.status === 200) {
+
+                onSuccses(xhr.response);
+
+            } else {
+                console.log('Ошибка! Код: ' + xhr.status);
+            }
+        });
+
+        xhr.addEventListener('error', function() {
+            onError('Ошибка! Код: ' + xhr.status);
+        });
+
+        xhr.addEventListener('timeout', function() {
+            onError('Время ожидания ответа истекло');
+        });
+
+    };
+
+
+})();
